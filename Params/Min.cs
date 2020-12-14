7108d5ac261c0f26930efbc54e0f49f5ebe3cb83
OlgaProtasova <protasolga2807@gmail.com>
2020-12-14 18:19:10 +0200

    Params

---
+++ Params/Min.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Params
+{
+    class Min
+    {
+        public int MinValue(params int[] nums)
+        {
+            if (nums.Length == 0)
+            {
+                Console.WriteLine("No nums");
+                return 0;
+            }
+
+            int m = nums[0];
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] < m)
+                {
+                    m = nums[i];
+                } 
+            }
+            return m;
+        }
+    }
+}
---
+++ Params/ParamsDemo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Params
+{
+    class ParamsDemo
+    {
+        static void Main(string[] args)
+        {
+            Min obj = new Min();
+            int min;
+            int a = 10, b = 5;
+            int[] mas = { 2, 4, 1, -5, 5 };
+                
+            min = obj.MinValue();
+            Console.WriteLine("0 min = " + min);
+
+            min = obj.MinValue(a, b);
+            Console.WriteLine("a, b min = " + min);
+
+            min = obj.MinValue(a, b, -1);
+            Console.WriteLine("a, b, -1 min = " + min);
+
+            min = obj.MinValue(mas);
+            Console.WriteLine("mas min = " + min);
+        }
+    }
+}
